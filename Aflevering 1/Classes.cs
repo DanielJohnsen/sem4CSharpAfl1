254cda2b15bba88a97131f9b3d7b1e8cd555d9e8
unknown <daniel.oestergaard.johnsen@gmail.com>
2020-03-06 09:27:49 +0100

    finished solution

---
+++ Aflevering 1/Classes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aflevering_1
+{
+    class User
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public int score { get; set; }
+        
+        public User(string s)
+        {
+            var L = s.Split(';');
+            id = L[0];
+            name = L[1];
+            age = int.Parse(L[2]);
+            score = int.Parse(L[3]);
+        }
+
+        public string getListBoxInfo()
+        {
+            return "name: " + name + " id: " + id;
+        }
+    }
+}
---
+++ Aflevering 1/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Aflevering_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<User> users = new List<User>();
+        int count = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItemOpen_Click(object sender, RoutedEventArgs e)
+        {
+            users.Clear();
+            count = 0;
+            UserBox.Items.Clear();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (string line in File.ReadLines(openFileDialog.FileName)){
+                    User u = new User(line);
+                    users.Add(u);
+                    count++;
+                }
+            }
+
+            UserBox.ItemsSource = users;
+
+            sbLineCount.Text = "Usercount: " + count;
+            sbLastRead.Text = "Last updated: " + DateTime.Now.ToString();
+
+        }
+
+        private void UserBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var index = UserBox.SelectedIndex;
+            lblAge.Content = "Age: " + users[index].age;
+            lblName.Content = "Name: " + users[index].name;
+            lblScore.Content = "Score: " + users[index].score;
+            lblId.Content = "Id: " + users[index].id;
+        }
+    }
+}
